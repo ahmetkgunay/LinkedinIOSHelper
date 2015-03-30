09e0f31cf509df125462d2246461403ec97f6a15
ahmetgunay <ahmet@mobven.com>
2015-03-30 15:46:44 +0300

    initial commit

---
+++ LinkedInIOSHelper/Constants/ContactInformationFields.m
@@ -0,0 +1,20 @@
+//
+//  ContactInformationFields.m
+//  linkedinDemo
+//
+//  Created by Ahmet Kazım Günay on 25/03/15.
+//  Copyright (c) 2015 ahmetkgunay. All rights reserved.
+//
+
+#import "ContactInformationFields.h"
+
+@implementation ContactInformationFields
+
+NSString * const phone_numbers              = @"phone-numbers";
+NSString * const bound_account_types        = @"bound-account-types";
+NSString * const im_accounts                = @"im-accounts";
+NSString * const main_address               = @"main-address";
+NSString * const twitter_accounts           = @"twitter-accounts";
+NSString * const primary_twitter_account    = @"primary-twitter-account";
+
+@end
---
+++ LinkedInIOSHelper/Constants/NetworkFields.m
@@ -0,0 +1,16 @@
+//
+//  NetworkFields.m
+//  Demo
+//
+//  Created by Ahmet Kazım Günay on 27/03/15.
+//  Copyright (c) 2015 ahmetkgunay. All rights reserved.
+//
+
+#import "NetworkFields.h"
+
+@implementation NetworkFields
+
+NSString * const connections = @"connections";
+
+
+@end
